56c2fea4ecec208b5ecc88c90c40f0717b0bc33a
Shu Mai <ansiboy@163.com>
2020-01-24 19:53:50 +0800

    重构  Commponet，剥离客户端组件对 react 依赖

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import '../lib/jquery.event.drop.live-2.2';
 
 
 export { proptDisplayNames as strings, proptDisplayNames } from "./common";
-export { Component, MasterPage, MasterPageContext, ComponentProps, PlaceHolder, PageView } from "./component";
+export { Component, ComponentProps } from "./component";
 export { ComponentPanel } from "./component-panel";
 export { EditorPanel, EditorPanelProps } from "./editor-panel";
 export { ComponentDefine, ComponentData } from "./models";
@@ -14,7 +14,7 @@ export { PageDesigner } from "./page-designer";
 export { PropEditor, PropEditorState, TextInput, DropDownItem, } from "./prop-editor";
 export { PropertyEditorInfo } from "./property-editor";
 export { classNames } from "./style";
-export { PageBuilderContext } from "./page-builder";
+export { PageBuilderContext, MasterPage, MasterPageContext, PlaceHolder, PageView } from "./react-page-builder";
 
 
 
