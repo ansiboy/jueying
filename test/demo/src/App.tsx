7242a6b0ac8f44fa9eb56e93388b3381fb83a0f9
maishu <ansiboy@163.com>
2022-12-02 15:22:04 +0800

    update

--- test/demo/src/App.tsx
+++ test/demo/src/App.tsx
@@ -1,14 +1,15 @@
-import './App.css';
 import { PageData } from 'maishu-jueying-core';
 import { PageDesigner, ComponentDiagram, ComponentPanel } from "maishu-jueying/out"
 import { componentsConfig } from "./components-config"
+import "maishu-jueying/out/style"
 
 function App() {
 
   let ids = {
     image1: "image1",
     div1: "div1",
-    div2: "div2"
+    div2: "div2",
+    button: "button"
   }
 
   let hello = "hello"
@@ -17,14 +18,17 @@ function App() {
     id: "simple",
     children: [
       { id: ids.div1, type: "div", props: {}, children: [hello] },
-      { id: ids.div2, type: "div", props: {}, children: [word] }
+      { id: ids.div2, type: "div", props: {}, children: [word] },
+      { id: ids.button, type: "Button", props: { clickedText: "HelloWorld" } }
     ]
   }
 
-  return <PageDesigner pageData={pageData1} componentsConfig={componentsConfig}>
-    <ComponentPanel />
-    <ComponentDiagram />
-  </PageDesigner>
+  return <div className='container'>
+    <PageDesigner pageData={pageData1} componentsConfig={componentsConfig}>
+      <ComponentPanel />
+      <ComponentDiagram />
+    </PageDesigner>
+  </div>
 }
 
 export default App;
--- test/demo/src/components-config.ts
+++ test/demo/src/components-config.ts
@@ -5,10 +5,14 @@ export let componentsConfig: ComponentsConfig = {}
 
 componentsConfig[typeNames.image] = {
     type: import("./components/image"),
-    editor: import("./editors/image")
+    editor: import("./editors/image"),
+    displayName: "图片",
+    icon: "glyphicon glyphicon-picture"
 }
 
 componentsConfig[typeNames.button] = {
     type: import("./components/button"),
     editor: import("./editors/button"),
+    displayName: "按钮",
+    icon: "glyphicon glyphicon-tag"
 }
